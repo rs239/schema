aaec7e45698e1cfefd4c5807aa95845e975cb714
Rohit Singh <rsingh@dragon.csail.mit.edu>
2020-09-29 01:15:50 -0400

    blah

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ import sys
 source_suffix = '.rst'
 master_doc = 'index'
 
-html_theme = {'logo_only': True}
+html_theme_options['logo_only'] = True
 
 html_static_path = ['_static']
 html_logo = '_static/Schema-webpage-logo-2-blue.png'
