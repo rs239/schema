e0a675b34821bce2d60f80ee3ace069f468035d6
Rohit Singh <rsingh@dragon.csail.mit.edu>
2020-09-29 12:21:40 -0400

    blah

--- docs/conf.py
+++ docs/conf.py
@@ -8,16 +8,13 @@ import sys
 from pathlib import Path
 from datetime import datetime
 
-import matplotlib  # noqa
-
-# Don’t use tkinter agg when importing scanpy → … → matplotlib
-matplotlib.use('agg')
+# import matplotlib  
+# matplotlib.use('agg')
 
 HERE = Path(__file__).parent
 sys.path[:0] = [str(HERE.parent), str(HERE / 'extensions')]
 
 import schema
-
 
 templates_path = ['_templates']
 
