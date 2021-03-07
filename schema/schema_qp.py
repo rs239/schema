96b15ea7783e6ede4e41b3b80391ca117dd3367e
Rohit Singh <rsingh@hydra.csail.mit.edu>
2021-03-07 01:55:07 -0500

    blah

--- schema/schema_qp.py
+++ schema/schema_qp.py
@@ -28,52 +28,9 @@ sys.path = copy.copy(oldpath)
 class SchemaQP:
     """Schema is a tool for integrating simultaneously-assayed data modalities
 
-Schema is a general algorithm for integrating heterogeneous data
-modalities. While it has been specially designed for multi-modal
-single-cell biological datasets, it should work in other multi-modal
-contexts too.
-
-This class provides a sklearn type fit+transform API for constrained
-affine transformations of input datasets such that the transformed data is
-in agreement with all the input datasets.
-
-It is described in the paper “Schema: metric learning enables
-interpretable synthesis of heterogeneous single-cell modalities" 
-(http://doi.org/10.1101/834549)
-
-Source code available at: https://github.com/rs239/schema
-
-
-:Example: Correlate gene expression with developmental stage. We demonstrate use with Anndata objects here.
-.. code-block:: Python
-
-    sqp = SchemaQP() # initialize with default params (min_corr = 0.99)
-    mod_X = sqp.fit_transform( adata.X, [ adata.obs['stage'] ]) # correlate the gene expression with the 'stage' parameter
-    gene_wts = sqp.feature_weights() # get a ranking of gene wts important to the correlation
-
-
-:Example: Correlate gene expression with three secondary modalities.
-.. code-block:: Python
-
-    sqp = SchemaQP(min_corr = 0.9) # lower than the default, allowing greater distortion of the primary modality 
-    sqp.fit( adata.X,    
-                 [ adata.obs['col1'], adata.obs['col2'], adata.obsm['Matrix1'] ], 
-                 [ "categorical", "numeric", "feature_vector"]) # data types of the three modalities
-    mod_X = sqp.transform( adata.X) # transform
-    gene_wts = sqp.feature_weights() # get gene importances
-
-
-:Example: Correlate gene expression 1) positively with ATAC-Seq data and 2) negatively with Batch information::
-.. code-block:: Python
-
-    atac_30d = sklearn.decomposition.TruncatedSVD(50).fit_transform( atac_cnts_sp_matrix)
-    sqp = SchemaQP(min_corr=0.9)
-    # df is a pd.DataFrame, srs is a pd.Series, -1 means try to disagree
-    mod_X = sqp.fit_transform( df_gene_exp, # gene expression dataframe
-                               [ atac_30d, batch_id],  # batch_info can be a Pandas Series or numpy array
-                               [ 'feature_vector', 'categorical'], 
-                               [ 1, -1]) # maximize combination of (agreement with ATAC-seq + disagreement with batch_id)
-    gene_wts = sqp.feature_weights() # get gene importances
+    This class provides a sklearn type fit+transform API for constrained
+    affine transformations of input datasets such that the transformed data is
+    in agreement with all the input datasets.
 """
 
     
