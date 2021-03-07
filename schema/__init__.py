8190cbec217259413f695491c4785cc350819f3b
Rohit Singh <rsingh@hydra.csail.mit.edu>
2021-03-07 13:57:54 -0500

    blah

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,7 +1,7 @@
 
 from .schema_qp import SchemaQP
 from .schema_base_config import schema_loglevel
-import .datasets as datasets
+import .datasets
 
 __all__ = ['SchemaQP', 'schema_loglevel', 'datasets']
 
