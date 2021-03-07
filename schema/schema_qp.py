bb95e6f33c7f77ad665c1e5cc6b6060d77b8a6f6
Rohit Singh <rsingh@hydra.csail.mit.edu>
2021-03-06 23:45:57 -0500

    blah

--- schema/schema_qp.py
+++ schema/schema_qp.py
@@ -44,40 +44,33 @@ interpretable synthesis of heterogeneous single-cell modalities"
 Source code available at: https://github.com/rs239/schema
 
 
-:Example:
+:Example: (Quick start) correlate gene expression with developmental stage. We demonstrate use with Anndata objects here::
 
-Quick start: correlate gene expression with developmental stage. We demonstrate use with Anndata objects here::
-
-    >>>>  sqp = SchemaQP() # initialize with default params (min_corr = 0.99)
-    >>>>  mod_X = sqp.fit_transform( adata.X, [ adata.obs['stage'] ]) # correlate the gene expression with the 'stage' parameter
-    >>>>  gene_wts = sqp.feature_weights() # get a ranking of gene wts important to the correlation
+    sqp = SchemaQP() # initialize with default params (min_corr = 0.99)
+    mod_X = sqp.fit_transform( adata.X, [ adata.obs['stage'] ]) # correlate the gene expression with the 'stage' parameter
+    gene_wts = sqp.feature_weights() # get a ranking of gene wts important to the correlation
 
 
-:Example:
+:Example: Correlate gene expression with three secondary modalities::
 
-Correlate gene expression with three secondary modalities::
-
-    >>>>  sqp = SchemaQP(min_corr = 0.9) # lower than the default, allowing greater distortion of the primary modality 
-    >>>>  sqp.fit( adata.X,    
+    sqp = SchemaQP(min_corr = 0.9) # lower than the default, allowing greater distortion of the primary modality 
+    sqp.fit( adata.X,    
                  [ adata.obs['col1'], adata.obs['col2'], adata.obsm['Matrix1'] ], 
                  [ "categorical", "numeric", "feature_vector"]) # data types of the three modalities
-    >>>>  mod_X = sqp.transform( adata.X) # transform
-    >>>>  gene_wts = sqp.feature_weights() # get gene importances
+    mod_X = sqp.transform( adata.X) # transform
+    gene_wts = sqp.feature_weights() # get gene importances
 
 
-:Example:
+:Example: Correlate gene expression 1) positive with ATAC-Seq data and 2) negatively with Batch information::
 
-Correlate gene expression 1) positive with ATAC-Seq data and 2) negatively with Batch information::
-
-    >>>>  atac_30d = sklearn.decomposition.TruncatedSVD(50).fit_transform( atac_cnts_sp_matrix)
-    >>>>  sqp = SchemaQP(min_corr=0.9)
-    >>>>  # df is a pd.DataFrame, srs is a pd.Series, -1 means try to disagree
-    >>>>  mod_X = sqp.fit_transform( df_gene_exp, # gene expression dataframe
-                                   [ atac_30d, batch_id],  # batch_info can be a Pandas Series or numpy array
-                                   [ 'feature_vector', 'categorical'], 
-                                   [ 1, -1]) # maximize combination of (agreement with ATAC-seq + disagreement with batch_id)
-    >>>> gene_wts = sqp.feature_weights() # get gene importances
-
+    atac_30d = sklearn.decomposition.TruncatedSVD(50).fit_transform( atac_cnts_sp_matrix)
+    sqp = SchemaQP(min_corr=0.9)
+    # df is a pd.DataFrame, srs is a pd.Series, -1 means try to disagree
+    mod_X = sqp.fit_transform( df_gene_exp, # gene expression dataframe
+                               [ atac_30d, batch_id],  # batch_info can be a Pandas Series or numpy array
+                               [ 'feature_vector', 'categorical'], 
+                               [ 1, -1]) # maximize combination of (agreement with ATAC-seq + disagreement with batch_id)
+    gene_wts = sqp.feature_weights() # get gene importances
 """
 
     
