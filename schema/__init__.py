9712bc812732c8c6985c0d1b325cede14b2ded6a
Rohit Singh <rsingh@hydra.csail.mit.edu>
2021-03-08 02:48:41 -0500

    blah

--- schema/__init__.py
+++ schema/__init__.py
@@ -4,16 +4,17 @@ from .schema_base_config import schema_loglevel
 
 __all__ = ['SchemaQP', 'schema_loglevel']
 
-from inspect import isclass
-from pkgutil import iter_modules
-from pathlib import Path
-from importlib import import_module
+
+import pkgutil, pathlib, importlib
+
+# from pkgutil import iter_modules
+# from pathlib import Path
+# from importlib import import_module
 
 # https://julienharbulot.com/python-dynamical-import.html
-#
 # iterate through the modules in the current package
-package_dir = Path(__file__).resolve().parent
-for (_, module_name, _) in iter_modules([package_dir]):
+#
+package_dir = pathlib.Path(__file__).resolve().parent
+for (_, module_name, _) in pkgutil.iter_modules([package_dir]):
     if 'datasets' in module_name:
-        # import the module and iterate through its attributes
-        module = import_module(f"{__name__}.{module_name}")
+        module = importlib.import_module(f"{__name__}.{module_name}")
