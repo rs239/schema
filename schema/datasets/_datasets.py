1a6929932b34bbd1530416a1862ba20a192b41b4
Rohit Singh <rsingh@hydra.csail.mit.edu>
2021-09-02 08:47:35 -0400

    blah

--- schema/datasets/_datasets.py
+++ schema/datasets/_datasets.py
@@ -31,7 +31,7 @@ def fly_brain():
     """ Anndata object containing scRNA-seq data of the ageing Drosophila brain (GSE107451, Davie et al., Cell 2018)
 """
     
-    adata = scanpy.read("datasets/Davie_fly_brain.h5", backup_url="http://schema.csail.mit.edu/datasets/Davie_fly_brain.h5")
+    adata = scanpy.read("datasets/Davie_fly_brain.h5", backup_url="http://cb.csail.mit.edu/cb/schema/datasets/Davie_fly_brain.h5")
     return adata
 
 
@@ -39,6 +39,6 @@ def scicar_mouse_kidney():
     """ Anndata object containing scRNA-seq+ATAC-seq data of mouse kidney cells from the Sci-CAR study (GSE117089, Cao et al., Science 2018)
 """
     
-    adata = scanpy.read("datasets/Cao_mouse_kidney.h5", backup_url="http://schema.csail.mit.edu/datasets/Cao_mouse_kidney.h5")
+    adata = scanpy.read("datasets/Cao_mouse_kidney.h5", backup_url="http://cb.csail.mit.edu/cb/schema/datasets/Cao_mouse_kidney.h5")
     return adata
 
